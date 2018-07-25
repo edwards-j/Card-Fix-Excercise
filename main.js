aa5fe9078f3030725b1a1b5d8d2a42a5c9ac3396
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-07-25 13:54:23 -0500

    You're now able to add cards back into the middle position

--- main.js
+++ main.js
@@ -32,15 +32,33 @@ const DragDropManager = Object.create(null, {
                     // Determine what's being dropped
                     const data = e.dataTransfer.getData("text")
 
-                    // Append card to target component as child
-                    // TODO: This should only happen if the target has no children nodes
-                    // TODO: This should not happen if the target is another stage card
+                    // Append card to target component as child, but only if there are no cards currently in there
                     if (e.target.hasChildNodes() !== true) {
                         e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
                     } else {
                         alert("You can't put that there")
                     }
                 }
+
+            })
+
+            const origin = document.querySelectorAll(".origin")
+
+            origin.forEach(target => {
+                // Dragover not supported by default. Turn that off.
+                target.ondragover = e => e.preventDefault()
+
+                target.ondrop = e => {
+                    // Enabled dropping on targets
+                    e.preventDefault()
+
+                    // Determine what's being dropped
+                    const data = e.dataTransfer.getData("text")
+
+                    // Append card to target component as child, but only if there are no cards currently in there
+                    e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+                }
+
             })
         }
     }
